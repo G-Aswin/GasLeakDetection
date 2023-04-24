69f888c8be8921de565af18d0a6569a267aa6a10
G. Aswin <aswin.g.rns@gmail.com>
2023-04-24 12:07:33 +0530

    Using all CSV files now + using pandas

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO
-import time, csv
+import time, csv, os
+import pandas as pd
 
 
 
@@ -14,16 +15,37 @@ def unpack_csv(filename):
     lines = csv.reader(myfile)
     return lines
 
-alldata = unpack_csv('ProcessedData/sample.csv')
-header_info = next(alldata)
+def aggregate_processed_data():
+    dflist = []
+    # All processed data present in Data/ProcessedRawData with a format of .*_dat.csv
+    for filename in os.listdir('Data/ProcessedRawData'):
+        if '_dat.csv' in filename:
+            df = pd.read_csv('Data/ProcessedRawData/' + filename)
+            dflist.append(df)
+            
+    combined_df = pd.concat(dflist, ignore_index=True)
+    return combined_df
+
+cdf = aggregate_processed_data()
+i = 0
+header_info = cdf.columns
+
 
 
 @socket.on('message')
 def handlemsg(msg):
+    global i
     time.sleep(1)
-    new_msg = next(alldata)
+    new_msg = cdf.iloc[i].tolist()
+    mod_new_msg = []
+    for ele in new_msg:
+        mod_new_msg.append(str(ele))
+    new_msg = mod_new_msg
+    print("********************************************************************************")
+    print(new_msg, type(new_msg))
+    i += 1
     socket.send(new_msg)
-    print('-I- Sent: ')
+    print('-I- Sent: ', new_msg)
         
 @app.route('/')
 def main():
@@ -32,3 +54,5 @@ def main():
 
 if __name__ == '__main__':
     socket.run(app)
+    # print(cdf.sample(n=3))
+    # print(cdf.info())
